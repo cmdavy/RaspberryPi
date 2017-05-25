178210457c56f7ff6c38b42e38c4692c6daa56ad
Chris Davy <chrisdavy@marlin.reefnet>
2017-05-24 23:09:23 -0400

    Initial chekcin

---
+++ src/main/java/com/cmd/cmdrasp/Application.java
@@ -0,0 +1,16 @@
+package com.cmd.cmdrasp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by chrisdavy on 5/24/17.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/com/cmd/cmdrasp/controller/LedController.java
@@ -0,0 +1,32 @@
+package com.cmd.cmdrasp.controller;
+
+import com.pi4j.io.gpio.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by chrisdavy on 5/24/17.
+ */
+public class LedController {
+
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping("/")
+    public String greeting()
+    {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/light")
+    public String light()
+    {
+        if (pin == null) {
+            GpioController gpioController = GpioFactory.getInstance();
+            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.LOW);
+        }
+
+        pin.toggle();
+
+        return "OK";
+    }
+
+}
