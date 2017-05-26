b250a69f2b315eaca3095628be99e0bb7b3b2bcd
cmd <cmdavy@gmail.com>
2017-05-25 23:11:52 -0400

    testing

--- src/main/java/com/cmd/cmdrasp/Application.java
+++ src/main/java/com/cmd/cmdrasp/Application.java
@@ -2,11 +2,13 @@ package com.cmd.cmdrasp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * Created by chrisdavy on 5/24/17.
  */
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main (String[] args)
