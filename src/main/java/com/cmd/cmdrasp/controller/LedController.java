38308c32b3a6fb0ae30dfdb4f684cb45ed826ae4
cmd <cmdavy@gmail.com>
2017-05-28 00:15:12 -0400

    testing

--- src/main/java/com/cmd/cmdrasp/controller/LedController.java
+++ src/main/java/com/cmd/cmdrasp/controller/LedController.java
@@ -15,7 +15,6 @@ import java.util.HashMap;
  * Created by chrisdavy on 5/24/17.
  */
 @RestController
-@RequestMapping("/")
 @Api(value="Raspberry Pi Led Application")
 public class LedController {
 
