a0bd45e2b6be6d14274f89b6b9e50db7d2301a1f
cmd <cmdavy@gmail.com>
2017-05-27 23:26:41 -0400

    testing

--- src/main/java/com/cmd/cmdrasp/Application.java
+++ src/main/java/com/cmd/cmdrasp/Application.java
@@ -15,7 +15,6 @@ import java.util.concurrent.Executor;
  */
 @SpringBootApplication
 @EnableAsync
-@EnableSwagger2
 public class Application extends AsyncConfigurerSupport{
 
     public static void main (String[] args)
