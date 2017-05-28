7f8a07995e8914322b412d1f5e909dc942e47f9c
cmd <cmdavy@gmail.com>
2017-05-27 23:41:09 -0400

    testing

--- src/main/java/com/cmd/cmdrasp/Application.java
+++ src/main/java/com/cmd/cmdrasp/Application.java
@@ -1,11 +1,15 @@
 package com.cmd.cmdrasp;
 
-import com.cmd.cmdrasp.configuration.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.concurrent.Executor;
@@ -15,6 +19,7 @@ import java.util.concurrent.Executor;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableSwagger2
 public class Application extends AsyncConfigurerSupport{
 
     public static void main (String[] args)
@@ -32,4 +37,13 @@ public class Application extends AsyncConfigurerSupport{
         executor.initialize();
         return executor;
     }
+
+    @Bean
+    public Docket ledApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select().apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
 }
