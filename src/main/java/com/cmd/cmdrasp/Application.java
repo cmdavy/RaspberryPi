3ebb1462156825d7f4a16c7c105a642c6376e551
cmd <cmdavy@gmail.com>
2017-05-27 23:57:58 -0400

    testing

--- src/main/java/com/cmd/cmdrasp/Application.java
+++ src/main/java/com/cmd/cmdrasp/Application.java
@@ -2,15 +2,9 @@ package com.cmd.cmdrasp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.concurrent.Executor;
@@ -18,7 +12,6 @@ import java.util.concurrent.Executor;
 /**
  * Created by chrisdavy on 5/24/17.
  */
-@Configuration
 @SpringBootApplication
 @EnableAsync
 @EnableSwagger2
@@ -38,15 +31,6 @@ public class Application extends AsyncConfigurerSupport{
         executor.setThreadNamePrefix("RaspLookup-");
         executor.initialize();
         return executor;
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build();
     }
 
 }
